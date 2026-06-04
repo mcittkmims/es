399e51c1ca655b2c5a73c2152d1b4d6ea62c13cb
mcittkmims <adrian.vremere.swe@gmail.com>
2026-06-04 14:18:17 +0300

    Use normalized website dataset

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const DATA_URL = "embedded_systems_exam_resolved_sei_style_package/embedded_systems_exam_resolved_variants_sei_style.json";
+export const DATA_URL = "data/si_variants_exam.json";
